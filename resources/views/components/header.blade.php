e50167026125917b05232354e941eeda8b22c301
Aphinan <154401387+shokun580@users.noreply.github.com>
2025-03-13 11:30:27 +0700

    feat(view)  : เพิ่มเมนู

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<div class="d-flex justify-content-between align-items-center px-3 py-3 border-bottom">
+    <!-- ด้านซ้าย -->
+    <span class="fw-semibold fs-4 text-dark">ภาพรวม</span>
+
+    <!-- ด้านขวา -->
+    <div class="d-flex align-items-center">
+        <img src="{{ asset('public/assets/img/user2-160x160.jpg') }}" alt="User"
+            class="rounded-circle " style="width: 30px; height: 30px; margin-right: 5px;">
+        <span class="fw-light fs-6 text-dark">Volunteer01</span>
+    </div>
+</div>
